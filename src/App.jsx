eb9e4be03f1e1da1996935d844d15b67baa4dccd
Monika Januszek <monika.januszek.mail@gmail.com>
2024-08-13 13:54:25 +0200

    first cofig & fonts added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
-
 import "./App.css";
+import Header from "./Header";
 
 function App() {
-  return <div className="wrapper">hello</div>;
+  return (
+    <div className="wrapper">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import "./styles/header.scss";
+
+function Header() {
+  return (
+    <div className="header-container" id="header">
+      <h1>All That Jazz</h1>
+    </div>
+  );
+}
+
+export default Header;
