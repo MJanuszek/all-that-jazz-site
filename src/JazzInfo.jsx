b9cfec910efde8f3872fc5e39d515c2ec4b57055
Monika Januszek <monika.januszek.mail@gmail.com>
2024-09-24 16:44:53 +0200

    indroduction section added and styled

--- src/JazzInfo.jsx
+++ src/JazzInfo.jsx
@@ -3,7 +3,40 @@ import "./styles/jazzInfo.scss";
 function JazzInfo() {
   return (
     <div className="jazz-info" id="jazzInfo">
-      jazzInfo
+      <h1 className="introduction-title">Jazz: A Living History</h1>
+      <p className="txt-section">
+        Jazz music, an art form that originated in the United States in the late
+        19th and early 20th centuries, embodies a unique blend of creativity,
+        complexity, and cultural fusion. It emerged from the African American
+        communities, primarily in the Southern United States, drawing on a rich
+        mix of African rhythms, European harmonic structure, and the blues. Jazz
+        distinguishes itself by its emphasis on improvisation, rhythmic urgency,
+        and a constant quest for innovation and expression.
+      </p>
+      <p className="txt-section">
+        Jazz musician’s ability to spontaneously create and vary melodies is
+        paramount. This improvisational feature means that no two performances
+        of a jazz piece are ever the same, giving the genre its ever-evolving
+        character. The interaction among musicians during a performance, where
+        they play both with and against each other in a form of musical
+        conversation, showcases the genre’s dynamic and collaborative nature.
+      </p>
+      <p className="txt-section">
+        Jazz has had a profound influence on other music genres, from rock and
+        pop to classical and folk, demonstrating its versatility and wide
+        appeal. Its reach has also been global, affecting music scenes and
+        inspiring musicians around the world. The improvisational skills,
+        rhythmic complexity, and harmonic innovations of jazz have enriched the
+        global music tapestry, making it a pivotal influence on 20th-century
+        music.
+      </p>
+      <p className="txt-section">
+        Today, jazz continues to thrive, adapting to contemporary musical trends
+        while retaining its identity and historical significance. It remains a
+        powerful vehicle for artistic exploration, cultural expression, and
+        social commentary, reflecting the ongoing story of human creativity.
+      </p>
+      <div className="img-introduction"></div>
     </div>
   );
 }
