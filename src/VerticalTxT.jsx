77dda9761a3b63e0fabd5a07aa19e207acfa4758
Monika Januszek <monika.januszek.mail@gmail.com>
2024-10-24 18:06:56 +0200

    css corrections

--- src/VerticalTxT.jsx
+++ src/VerticalTxT.jsx
@@ -2,7 +2,7 @@ import "./styles/legends.scss";
 
 function VerticalTxT({ text }) {
   const letters = text.split("").map((letter, index) => (
-    <span key={index} style={{ display: "block" }} className="title">
+    <span key={index} style={{ display: "block" }} className="title legends-h1">
       {letter}
     </span>
   ));
